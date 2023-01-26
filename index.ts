28bddb234895c702cf4f009573f7d0cad59b88f0
Sean Emerson <emerson.sean.dev@gmail.com>
2023-01-26 20:10:55 +0200

    broken

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 /**
  * Export astro components
  * usage:
- * import { ComponentName } from 'a/libstro-bootstrap';
+ * import { ComponentName } from 'astro-bootstrap';
  */
+
+// Alert
+export * as Alert from './lib/components/Alert';
+// export { default as AlertComponent } from './lib/components/AlertComponent.astro';
+export { default as AlertHeading } from './lib/components/AlertHeading.astro';
+export { default as AlertLink } from './lib/components/AlertLink.astro';
 
 // Accordion
 export { default as Accordion } from './lib/components/Accordion.astro';
@@ -18,10 +24,6 @@ export { default as Breadcrumb } from './lib/components/Breadcrumb.astro';
 export { default as BreadcrumbItem } from './lib/components/BreadcrumbItem.astro';
 
 // Navbar Components
-// import * as Navbar from './lib/components/Navbar';
-// export { Navbar };
-// to allow Navbar.Item etc..
-// if it works try this below
 export * as Navbar from './lib/components/Navbar';
 export { default as NavbarCollapse } from './lib/components/NavbarCollapse.astro';
 export { default as NavbarDropdown } from './lib/components/NavbarDropdown.astro';
@@ -37,3 +39,6 @@ export { default as NavTabs } from './lib/components/NavTabs.astro';
 export { default as TabContent } from './lib/components/TabContent.astro';
 export { default as TabPane } from './lib/components/TabPane.astro';
 export { default as Tabs } from './lib/components/Tabs.astro';
+
+// Utils
+
---
+++ lib/components/Alert.ts
@@ -0,0 +1,7 @@
+
+// export { default as Heading } from './AlertHeading.astro';
+// export { default as Link } from './AlertLink.astro';
+export { default as Component } from './AlertComponent.astro';
+
+import { default as AlertComponent } from './AlertComponent.astro';
+export default AlertComponent;
