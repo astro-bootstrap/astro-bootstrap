c562e1dccd41d53f329c8e3a791f1a850fca0888
Sean Emerson <emerson.sean.dev@gmail.com>
2023-01-29 17:59:02 +0200

    update exports

--- lib/components/Alert.ts
+++ lib/components/Alert.ts
@@ -1,7 +1,4 @@
-
-// export { default as Heading } from './AlertHeading.astro';
-// export { default as Link } from './AlertLink.astro';
-export { default as Component } from './AlertComponent.astro';
-
 import { default as AlertComponent } from './AlertComponent.astro';
 export default AlertComponent;
+export { default as Heading } from './AlertHeading.astro';
+export { default as Link } from './AlertLink.astro';
