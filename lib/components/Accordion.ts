7370d3246ae0e60e3f893f6ca14d72e4ee0d73f7
Sean Emerson <emerson.sean.dev@gmail.com>
2023-02-07 22:20:49 +1100

    fix export

--- lib/components/Accordion.ts
+++ lib/components/Accordion.ts
@@ -1,6 +1,6 @@
 import Accordion from './Accordion.astro';
-import Body  from './AccordionBody.astro';
-import Header  from './AccordionHeader.astro';
+import Body from './AccordionBody.astro';
+import Header from './AccordionHeader.astro';
 import Item from './AccordionItem.astro';
 
-export default Object.assign(Accordion, {Body, Header, Item});
+export default Object.assign(Accordion, { Body, Header, Item });
