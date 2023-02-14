d570b970449dc23c8818668e0a6bd83fb14bea7f
Sean Emerson <emerson.sean.dev@gmail.com>
2023-02-15 09:33:23 +1100

    in progress no pagination

--- www/src/config.ts
+++ www/src/config.ts
@@ -3,9 +3,9 @@ export const SITE_TITLE = 'Astro-Bootstrap';
 
 export const MAIN_MENU: NavbarItemType[] = [
   { text: 'Home', href: '/' },
-  { text: 'Getting Started', href: '/getting-started' },
+  { text: 'Getting Started', href: '/getting-started', parent: true },
   { text: 'Components', href: '/components', parent: true},
-  { text: 'Custom Utilities', href: '/utilities#sideUtilities', parent: true },
+  { text: 'Custom Utilities', href: '/components#sideUtilities', parent: true },
   // {
   //   title: 'parent',
   //   subItems: [{ title: 'child', href: '/' }, { divider: true }, { title: 'child2', href: '/' }],
