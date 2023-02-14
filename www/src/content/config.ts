ae86aab316f30c9581d2ab41908de5d99907d670
Sean Emerson <emerson.sean.dev@gmail.com>
2023-02-15 01:32:12 +1100

    pagination broken due to collections

--- www/src/content/config.ts
+++ www/src/content/config.ts
@@ -1,18 +1,18 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
-const componentsCollection = defineCollection({ 
+const components = defineCollection({ 
   schema: z.object({
     title: z.string(),
-    description: z.string(),
-    layout: z.string(),
-    variant: z.string().optional()
+    description: z.string().optional(),
+    draft: z.boolean().optional(),
+    variant: z.string().optional(),
+    docs: z.string().optional()
   }),
  });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'components': componentsCollection,
-  'utiltiies': componentsCollection,
+  components,
 };
